c97c5fb5cd3631ea8b8ce02fd05cb21329ed42cf
hongyi <xtwboboy@163.com>
2018-09-08 07:42:05 +0800

    test check and update

--- Demo1/main.cc
+++ Demo1/main.cc
@@ -8,25 +8,23 @@
  *
  * @return base raised to the power exponent.
  */
-double power(double base, int exponent)
-{
+double power(double base, int exponent) {
     int result = base;
     int i;
 
     if (exponent == 0) {
         return 1;
     }
-    
-    for(i = 1; i < exponent; ++i){
+
+    for (i = 1; i < exponent; ++i) {
         result = result * base;
     }
 
     return result;
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc < 3){
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
         printf("Usage: %s base exponent \n", argv[0]);
         return 1;
     }
